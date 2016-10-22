986d38c8e272bf3c8db8168bed1d0cabc846c909
digitalhawk <digitalhawk2016@gmail.com>
2016-10-22 16:18:21 +0530

    id change

--- js/script.js
+++ js/script.js
@@ -6,18 +6,18 @@ var admobid = {};
 // select the right Ad Id according to platform
 if( /(android)/i.test(navigator.userAgent) ) { 
     admobid = { // for Android
-        banner: 'ca-app-pub-6869992474017983/9375997553',
-        interstitial: 'ca-app-pub-6869992474017983/1657046752'
+        banner: 'ca-app-pub-3670000423095472/3826999342',
+        interstitial: 'ca-app-pub-3670000423095472/5303732548'
     };
 } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
     admobid = { // for iOS
-        banner: 'ca-app-pub-6869992474017983/4806197152',
+        banner: 'ca-app-pub-3670000423095472/3826999342',
         interstitial: 'ca-app-pub-6869992474017983/7563979554'
     };
 } else {
     admobid = { // for Windows Phone
-        banner: 'ca-app-pub-6869992474017983/8878394753',
-        interstitial: 'ca-app-pub-6869992474017983/1355127956'
+        banner: 'ca-app-pub-3670000423095472/3826999342',
+        interstitial: 'ca-app-pub-3670000423095472/5303732548'
     };
 }
 
@@ -37,18 +37,18 @@ var admobid = {};
 // select the right Ad Id according to platform
 if( /(android)/i.test(navigator.userAgent) ) { 
     admobid = { // for Android
-        banner: 'ca-app-pub-6869992474017983/9375997553',
-        interstitial: 'ca-app-pub-6869992474017983/1657046752'
+        banner: 'ca-app-pub-3670000423095472/3826999342',
+        interstitial: 'ca-app-pub-3670000423095472/5303732548'
     };
 } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
     admobid = { // for iOS
-        banner: 'ca-app-pub-6869992474017983/4806197152',
-        interstitial: 'ca-app-pub-6869992474017983/7563979554'
+        banner: 'ca-app-pub-3670000423095472/3826999342',
+        interstitial: 'ca-app-pub-3670000423095472/5303732548'
     };
 } else {
     admobid = { // for Windows Phone
-        banner: 'ca-app-pub-6869992474017983/8878394753',
-        interstitial: 'ca-app-pub-6869992474017983/1355127956'
+        banner: 'ca-app-pub-3670000423095472/3826999342',
+        interstitial: 'ca-app-pub-3670000423095472/5303732548'
     };
 }
 
